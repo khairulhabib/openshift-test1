6a312c2e45a1b9493590f26c752ac7ecc126a656
khairulhabib <mkhairulhabibm@gmail.com>
2017-02-13 10:33:09 +0700

    initial

---
+++ src/main/java/com/btpnsyariah/os/SpringbootOcApplication.java
@@ -0,0 +1,20 @@
+package com.btpnsyariah.os;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringbootOcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootOcApplication.class, args);
+	}
+
+	@GetMapping(value = "/hello")
+	public String hello(String hello){
+		return "Springboot " + hello;
+	}
+}
---
+++ src/test/java/com/btpnsyariah/os/SpringbootOcApplicationTests.java
@@ -0,0 +1,16 @@
+package com.btpnsyariah.os;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootOcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
